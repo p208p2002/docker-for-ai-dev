afd8689f2a014dd752309238937c83cfa7b5a420
Philip <p208p2002@gmail.com>
2020-02-26 07:47:48 +0800

    no message

--- cu10.0-py36-tf2.1.0-torch1.3-jupyter/script_files/create_user.sh
+++
@@ -1,7 +0,0 @@
-# setting jupyter pwd;
-sed -i '$ d' /root/.jupyter/jupyter_notebook_config.py;
-echo -e "c.NotebookApp.token = \x22${PASSWORD}\x22" >> /root/.jupyter/jupyter_notebook_config.py;
-# read ENV_VAR and create user;
-useradd -m $NAME -s /bin/bash;
-sudo adduser $NAME sudo;
-echo $NAME:$PASSWORD | chpasswd;
