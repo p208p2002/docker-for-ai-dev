0a2b60fdb63daf73a1965986e6e03b19b4e5fa97
Philip <p208p2002@gmail.com>
2020-02-26 10:21:55 +0800

    no message

---
+++ cu10.1-py36-tf2.1.0-torch1.4-jupyter-vscode/script_files/docker_start.sh
@@ -0,0 +1,19 @@
+# Run repeat
+service fail2ban start;
+service ssh start;
+(nohup /code-server2.1698-vsc1.41.1-linux-x86_64/code-server --host 0.0.0.0  2> /dev/null&);
+(nohup jupyter notebook --allow-root --ip=0.0.0.0 2> /dev/null&);
+
+# Run once, hold otherwise
+if [ -f "already_ran" ]; then
+    echo "Already ran the Entrypoint once. Holding indefinitely for debugging.";
+else
+    # setting jupyter pwd;
+    sed -i '$ d' /root/.jupyter/jupyter_notebook_config.py;
+    echo -e "c.NotebookApp.token = \x22${PASSWORD}\x22" >> /root/.jupyter/jupyter_notebook_config.py;
+    # read ENV_VAR and create user;
+    useradd -m $NAME -s /bin/bash;
+    sudo adduser $NAME sudo;
+    echo $NAME:$PASSWORD | chpasswd;
+fi
+touch already_ran;
