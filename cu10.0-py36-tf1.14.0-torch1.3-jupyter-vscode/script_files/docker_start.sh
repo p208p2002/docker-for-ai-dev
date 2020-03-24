65c5cf3288ed49fac7e8b56c1a98b1110f422ae7
Philip <p208p2002@gmail.com>
2020-03-24 12:54:47 +0800

    no message

--- cu10.0-py36-tf1.14.0-torch1.3-jupyter-vscode/script_files/docker_start.sh
+++ cu10.0-py36-tf1.14.0-torch1.3-jupyter-vscode/script_files/docker_start.sh
@@ -6,21 +6,11 @@ else
     useradd -m $NAME -s /bin/bash;
     sudo adduser $NAME sudo;
     echo $NAME:$PASSWORD | chpasswd;
-    # set root home as user home
-    echo HOME=/home/${NAME} >> /etc/environment;
-    source /etc/environment;
-    echo "cd ${HOME}" >> /root/.bashrc;
-    cp /root/.bashrc /home/${NAME}/
-    # jupyter config
-    rm -rf /home/${NAME}/.jupyter;
-    mkdir /home/${NAME}/.jupyter;
-    cp /root/.jupyter/jupyter_notebook_config.py /home/${NAME}/.jupyter/''
 fi
 touch already_ran;
 
 # Run repeat
-source /etc/environment;
 service fail2ban start;
 service ssh start;
-(nohup sh -c "/code-server2.1698-vsc1.41.1-linux-x86_64/code-server --host 0.0.0.0 /home/${NAME}" 2> /dev/null&);
-(nohup sh -c "jupyter notebook --allow-root --ip=0.0.0.0 --notebook-dir=/home/${NAME}" 2> /dev/null&);
+(nohup sh -c "/code-server2.1698-vsc1.41.1-linux-x86_64/code-server --host 0.0.0.0 ${HOME}" 2> /dev/null&);
+(nohup sh -c "jupyter notebook --allow-root --ip=0.0.0.0 --notebook-dir=${HOME}" 2> /dev/null&);
