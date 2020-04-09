af61e5843e348ab46f24d374f8f9d8da547e4fdf
Philip <p208p2002@gmail.com>
2020-04-09 11:04:21 +0800

    lf

---
+++ build/precache-manifest.e4b10de1bd56eb313a0035c64727599c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cb9d0a3fe11193600af90ee2a3ec85",
+    "url": "/docker-for-ai-dev/index.html"
+  },
+  {
+    "revision": "9fec31311cfffc360444",
+    "url": "/docker-for-ai-dev/static/css/main.3d724fb9.chunk.css"
+  },
+  {
+    "revision": "40afee7b0928f0fcd1e4",
+    "url": "/docker-for-ai-dev/static/js/2.4fdb24d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/docker-for-ai-dev/static/js/2.4fdb24d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fec31311cfffc360444",
+    "url": "/docker-for-ai-dev/static/js/main.b2c6b5a3.chunk.js"
+  },
+  {
+    "revision": "b9b6a4488b8c7bc13775",
+    "url": "/docker-for-ai-dev/static/js/runtime-main.801fcd5c.js"
+  }
+]);
--- cu10.1-py36-tf2.1.0-torch1.4-jupyter-vscode/script_files/docker_start.sh
+++ cu10.1-py36-tf2.1.0-torch1.4-jupyter-vscode/script_files/docker_start.sh
@@ -12,5 +12,5 @@ touch already_ran;
 # Run repeat
 service fail2ban start;
 service ssh start;
-(nohup sh -c "/code-server2.1698-vsc1.41.1-linux-x86_64/code-server --host 0.0.0.0 ${HOME}" 2> /dev/null&);
+(nohup sh -c "/code-server/code-server --host 0.0.0.0 ${HOME}" 2> /dev/null&);
 (nohup sh -c "jupyter notebook --allow-root --ip=0.0.0.0 --notebook-dir=${HOME}" 2> /dev/null&);
